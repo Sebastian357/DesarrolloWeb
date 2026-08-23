df6495548927a6401b3997771942473807164dbf
Sebastian Pintos <sebastianpintosit@gmail.com>
2026-08-23 17:55:04 -0300

    20260823 componente LoginForm

--- src/App.jsx
+++ src/App.jsx
@@ -1,53 +1,18 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
+
 import './App.css'
 
-import Encabezado from './componentes/loginform/encabezado'
-import CuadroDeTexto from './componentes/loginform/cuadrodetexto'
-import Boton from './componentes/loginform/boton'
-import MiLink from './componentes/loginform/milink'
-
+import LoginForm from './componentes/loginform/loginform'
 
 function App() {
-  
 
-  return (
-      <>
-      <div className="container text-center w-50 mt-5">
-            <div class="row">
-                <div class="col">
-                    <Encabezado />
-                </div>
-            
-            </div>
-            <div class="row">
-                <div class="col">
-                    <CuadroDeTexto Texto="Usuario" Tipo="text" />
-                </div>
-            </div>
-            <div class="row">
-                <div class="col">
-                    <CuadroDeTexto Texto="Contraseña" Tipo="password" />
-                </div>
-            </div>
-            <div class="row">
-                <div class="col">
-                    <Boton />
-                </div>
-            </div>
-            <div class="row">
-                <div class="col">
-                    <MiLink Texto="Registrarse" LinkRedireccion="https://www.instagram.com" />
-                    <MiLink Texto="¿Olvidaste tu contraseña?" LinkRedireccion="https://www.infobae.com" />
-                </div>
-            </div>
 
-        </div>
-      
-      </>
-  )
+    return (
+        <>
+            <LoginForm />
+
+        </>
+    )
 }
 
 export default App;
--- src/componentes/loginform/loginform.jsx
+++ src/componentes/loginform/loginform.jsx
@@ -1,6 +1,39 @@
+
+import CuadroDeTexto from "./cuadrodetexto.jsx";
+import Encabezado from "./encabezado.jsx";
+import MiLink from "./milink.jsx";
+import Boton from "./boton.jsx";
+
 const LoginForm = () => {
     return (
-        <h1>Poner en éste componente el formulario de login</h1>
+        <div className="container text-center w-50 mt-5">
+            <div class="row">
+                <div class="col">
+                    <Encabezado />
+                </div>
+            </div>
+            <div class="row">
+                <div class="col">
+                    <CuadroDeTexto Texto="Usuario" Tipo="text" />
+                </div>
+            </div>
+            <div class="row">
+                <div class="col">
+                    <CuadroDeTexto Texto="Contraseña" Tipo="password" />
+                </div>
+            </div>
+            <div class="row">
+                <div class="col">
+                    <Boton />
+                </div>
+            </div>
+            <div class="row">
+                <div class="col">
+                    <MiLink Texto="Registrarse" LinkRedireccion="https://www.instagram.com" />
+                    <MiLink Texto="¿Olvidaste tu contraseña?" LinkRedireccion="https://www.infobae.com" />
+                </div>
+            </div>
+        </div>
     )
 };
 export default LoginForm;
